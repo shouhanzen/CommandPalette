c9d8ce011f1dd902bc14b73579fca78ecec77d13
D0rkKnight <shouhanzen@gmail.com>
2023-12-05 18:25:00 -0800

    Setup

---
+++ frontend/main.js
@@ -0,0 +1,60 @@
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+// Keep a global reference of the window object to avoid it being garbage collected.
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false // Initially hide the window
+  });
+
+  // Load the index.html of the app.
+  win.loadFile('index.html');
+}
+
+function toggleWindow() {
+  if (win.isVisible()) {
+    win.hide();
+  } else {
+    win.show();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Register a global shortcut listener.
+  const ret = globalShortcut.register('Ctrl+Shift+I', () => {
+    toggleWindow();
+  });
+
+  if (!ret) {
+    console.log('Registration failed');
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('Ctrl+Shift+I'));
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // Unregister the global shortcut listener.
+  globalShortcut.unregisterAll();
+
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
