9f389ae074dd0ee66b51d7d866e8210ddede3166
D0rkKnight <shouhanzen@gmail.com>
2023-12-26 01:52:45 -0800

    Mysterious pywin32 bug w/ COM errors

--- backend/src/apps_interface.py
+++ backend/src/apps_interface.py
@@ -5,12 +5,16 @@ import os
 import src.extract_icon as extract_icon
 import configparser
 import shutil
+
 from PIL import Image
 from src.cmd_types import *
 
 
 def start_app(app: str):
     if platform.system() == "Windows":
+        # import pythoncom
+        # pythoncom.CoInitialize()
+
         command = f'start "" "{app}"'
         subprocess.Popen(command, shell=True)
     else:
@@ -176,31 +180,3 @@ def list_shortcuts_windows(directory):
                 out += [{"name": name, "path": real_path, "icon": icon}]
 
     return out
-
-
-# Courtesy of https://stackoverflow.com/questions/75040757/how-do-i-list-all-the-installed-applications-using-python, Florian EDEMESSI
-# def get_installed_programs():
-#     reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-#     key = winreg.OpenKey(reg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
-
-#     for i in range(winreg.QueryInfoKey(key)[0]):
-#         software_key_name = winreg.EnumKey(key, i)
-#         software_key = winreg.OpenKey(key, software_key_name)
-#         try:
-#             software_name = winreg.QueryValueEx(software_key, "DisplayName")[0]
-#             try:
-#                 # Try to get the executable path (DisplayIcon)
-#                 software_path = winreg.QueryValueEx(software_key, "DisplayIcon")[0]
-#             except FileNotFoundError:
-#                 try:
-#                     # If DisplayIcon not found, try InstallLocation
-#                     software_path = winreg.QueryValueEx(
-#                         software_key, "InstallLocation"
-#                     )[0]
-#                 except FileNotFoundError:
-#                     # If both are not found, set as not available
-#                     software_path = "Path not available"
-
-#             print(f"{software_name} - Path: {software_path}")
-#         except Exception as e:
-#             print(f"Error retrieving information for software index {i}: {e}")
